9c22979db9ccd155b8ae2d49256c5c0c0f39cf24
Sriraja2005 <154749369+Sriraja2005@users.noreply.github.com>
2025-09-27 08:40:17 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from decouple import config
 from pathlib import Path
 from datetime import timedelta
-from decouple import config, Csv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
